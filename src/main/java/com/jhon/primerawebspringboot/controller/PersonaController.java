1853d8d6e1b9ac309b00c945548d0a9e54f1c496
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-08-25 16:24:26 -0500

    Crear formulario nueva persona

--- src/main/java/com/jhon/primerawebspringboot/controller/PersonaController.java
+++ src/main/java/com/jhon/primerawebspringboot/controller/PersonaController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,7 +28,15 @@ public class PersonaController {
         return "listar";
     }
 
+    @GetMapping("/nueva")
     public String mostrarFormularioDeNuevaPersona(Model model){
-        return "crear";
+        model.addAttribute("persona", new Persona());
+        model.addAttribute("accion", "/personas/nueva");
+        return "formulario";
+    }
+
+    @PostMapping
+    public String guardarNuevaPersona(@ModelAttribute Persona persona){
+        personaService.CrearPersona(persona);
     }
 }
